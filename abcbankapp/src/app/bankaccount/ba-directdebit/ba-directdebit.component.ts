12d86a41ad7cfe0dcb644327a8e070bc2d4a644b
eswaribala <parameswaribala@gmail.com>
2023-03-16 15:00:00 +0530

    updated

--- abcbankapp/src/app/bankaccount/ba-directdebit/ba-directdebit.component.ts
+++ abcbankapp/src/app/bankaccount/ba-directdebit/ba-directdebit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Optional} from '@angular/core';
 import {concatAll, map, of} from "rxjs";
 import {fromFetch} from "rxjs/fetch";
+import {ImageService} from "../../services/image.service";
 
 @Component({
   selector: 'app-ba-directdebit',
@@ -11,22 +12,44 @@ export class BaDirectdebitComponent implements OnInit {
   message: any;
   retrievedImage: any;
   imageName: any;
+  private selectedFile: any;
+  private retrieveResponse: any;
+  private base64Data: any;
 
-  constructor() { }
+  constructor(@Optional() private imageService:ImageService) { }
 
   ngOnInit(): void {
 
   }
 
-  onFileChanged($event: Event) {
-    
+  onFileChanged($event: any) {
+    this.selectedFile = $event.target.files[0];
   }
 
   onUpload() {
-    
+    console.log(this.selectedFile);
+
+    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    //Make a call to the Spring Boot Application to save the image
+    this.imageService.sendImage(uploadImageData)
+      .subscribe((response) => {
+          console.log(response);
+        }
+      );
   }
 
   getImage() {
-    
+//Make a call to Sprinf Boot to get the Image Bytes.
+    this.imageService.receiveImage(this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResponse = res;
+          this.base64Data = this.retrieveResponse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
   }
 }
